e5eb3d1c4385b31b4a988e8a2061c8e50a5c1490
RicardoEgas <rj_egas@hotmail.com>
2023-10-20 14:32:36 +0200

    Add class Solver

---
+++ solver.rb
@@ -0,0 +1,29 @@
+class Solver
+  def factorial(n)
+    raise ArgumentError, 'Input must be a non-negative integer' if n.negative?
+
+    return 1 if n.zero?
+
+    (1..number).reduce(:*)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    raise ArgumentError, "Input must be a positive integer" unless n.is_a?(Integer) && n.positive?
+
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+
+    elsif (n % 5).zero?
+      'buzz'
+
+    else
+      n.to_s
+    end
+  end
+end
